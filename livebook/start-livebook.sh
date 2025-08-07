6cd79a98d6406fa2edfa6972fc4eba2691ea3551
fep <franpaez@gmail.com>
2025-08-07 00:30:35 -0300

    Livebook

---
+++ livebook/start-livebook.sh
@@ -0,0 +1,5 @@
+# In order to access and save notebooks directly to your machine
+# you can mount a local directory into the container.
+# Make sure to specify the user with "-u $(id -u):$(id -g)"
+# so that the created files have proper permissions
+docker run -p 8080:8080 -p 8081:8081 --pull always -u $(id -u):$(id -g) -v $(pwd):/data ghcr.io/livebook-dev/livebook
